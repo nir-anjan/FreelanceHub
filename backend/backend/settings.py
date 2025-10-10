95903ba2905b238518d9ebf9af541e8cc53ed3c1
nir-anjan <niranjan.s@neoito.com>
2025-10-11 02:06:58 +0530

    s

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'freelance_marketplace_db',
+        'USER':'postgres',
+        'PASSWORD':'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
